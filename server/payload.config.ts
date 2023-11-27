ccd7b3565e4f41f1dcacd31ed835cc30caa2d4d3
Nhat Nguyen <nhatnguyenktz119@gmail.com>
2023-11-27 14:59:00 +0700

    Model data & enable uploads

--- server/payload.config.ts
+++ server/payload.config.ts
@@ -6,6 +6,10 @@ import { slateEditor } from '@payloadcms/richtext-slate'
 import { webpackBundler } from '@payloadcms/bundler-webpack'
 
 import { Users } from '../collections/users'
+import { Products } from '../collections/products'
+import { Media } from '../collections/media'
+import { ProductFile } from '../collections/product-file'
+import { Orders } from '../collections/orders'
 
 dotenv.config({
   path: path.resolve(__dirname, '../.env'),
@@ -13,7 +17,7 @@ dotenv.config({
 
 export default buildConfig({
   serverURL: process.env.NEXT_PUBLIC_SERVER_URL || '',
-  collections: [Users],
+  collections: [Users, Products, Media, ProductFile, Orders],
   routes: {
     admin: '/sell',
   },
